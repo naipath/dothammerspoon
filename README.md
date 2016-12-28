b3e82afc48185f0d5379f048eccbfa558227e858
tvs <teije-vs@hotmail.com>
2016-12-28 21:31:34 +0100

    Initial setup hammerspoon config

---
+++ README.md
@@ -0,0 +1,12 @@
+# .Hammerspoon
+
+This is my .hammerspoon directory.
+
+The following shortcuts are supported:
+
+- `ctrl + ⌥ + ⌘  + left`  - move window to the left
+- `ctrl + ⌥ + ⌘  + right` - move window to the right
+- `ctrl + ⌥ + ⌘  + up` - maximize app
+- `ctrl + ⌥ + ⌘  + M` - toggle fullscreen mode
+- `ctrl + ⌥ + ⌘  + E` - show window hints
+- `⌥ + space` - switch between terminal and focussed app
---
+++ terminalswitch.lua
@@ -0,0 +1,14 @@
+local terminalApp = "com.apple.Terminal"
+local previousOpenedApp = ""
+
+-- Toggle between the terminal and the previously focussed app
+hs.hotkey.bind({"alt"}, "space", function()
+    local focussedApp = hs.window.focusedWindow():application():bundleID()
+    
+    if focussedApp ~= terminalApp then
+        previousOpenedApp = focussedApp
+        hs.application.launchOrFocusByBundleID(terminalApp)
+    elseif previousOpenedApp then
+        hs.application.launchOrFocusByBundleID(previousOpenedApp)
+    end
+end)
