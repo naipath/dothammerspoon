a102be98a9b70e448a613f2e51bae988d8946470
tvs <teije-vs@hotmail.com>
2017-02-14 14:22:03 +0100

    Changed to iterm, added that bundleId

--- terminalswitch.lua
+++ terminalswitch.lua
@@ -1,10 +1,11 @@
-local terminalApp = "com.apple.Terminal"
+-- local terminalApp = "com.apple.Terminal"
+local terminalApp = "com.googlecode.iterm2"
 local previousOpenedApp = ""
 
 -- Toggle between the terminal and the previously focussed app
 hs.hotkey.bind({"alt"}, "space", function()
     local focussedApp = hs.window.focusedWindow():application():bundleID()
-    
+
     if focussedApp ~= terminalApp then
         previousOpenedApp = focussedApp
         hs.application.launchOrFocusByBundleID(terminalApp)
