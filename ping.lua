a2a81eb4226b7de4c3f6c48395b7ab2fcd13ebd4
tvs <tvansloten@xebia.com>
2017-11-21 19:28:34 +0100

    Formatting terminalswitch, improved code ping.lua a bit

--- ping.lua
+++ ping.lua
@@ -2,8 +2,16 @@
 -- Use ping to check internet
 --
 
+local pingResponses = {
+	didFail = "failed",
+	sendPacketFailed = "failed",
+	receivedPacket = "success"
+}
+
 hs.hotkey.bind(cah, "p", function() 
-	hs.network.ping.ping("8.8.8.8", 2, 1, 2, "any", function(_, message) 
-		hs.alert.show(message)
+	hs.network.ping.ping("8.8.8.8", 1, 1, 0.5, "any", function(_, message)
+		if pingResponses[message] ~= nil then 
+			hs.alert.show(pingResponses[message])
+		end
 	end)
 end)
