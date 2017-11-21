b1fafe1ca82736457f99280fa2e8daeecc7fb34a
tvs <tvansloten@xebia.com>
2017-11-21 21:58:53 +0100

    Added apple notification instead of hs.alert

--- ping.lua
+++ ping.lua
@@ -2,16 +2,19 @@
 -- Use ping to check internet
 --
 
+local successNotification = "display notification \"> ping 8.8.8.8\" with title \"Successful\" sound name \"blow\""
+local errorNotification = "display notification \"> ping 8.8.8.8\" with title \"Failed\" sound name \"sosumi\""
+
 local pingResponses = {
-	didFail = "failed",
-	sendPacketFailed = "failed",
-	receivedPacket = "success"
+	didFail = errorNotification,
+	sendPacketFailed = errorNotification,
+	receivedPacket = successNotification
 }
 
 hs.hotkey.bind(cah, "p", function() 
 	hs.network.ping.ping("8.8.8.8", 1, 1, 0.5, "any", function(_, message)
-		if pingResponses[message] ~= nil then 
-			hs.alert.show(pingResponses[message])
+		if pingResponses[message] ~= nil then
+			hs.osascript.applescript(pingResponses[message])
 		end
 	end)
 end)
