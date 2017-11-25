ebd426460623b1ce5edcc13e201cf64e83f42278
tvs <tvansloten@xebia.com>
2017-11-25 23:13:58 +0100

    Added option to scroll up using hissing noise

--- noises.lua
+++ noises.lua
@@ -8,12 +8,21 @@ function bind(reference, callBack)
 	end
 end
 
+local modifier = -20
+
 local timer = hs.timer.new(0.01, function()
-	hs.eventtap.event.newScrollEvent({ 0, -20 }, {}, 'pixel'):post()
+	hs.eventtap.event.newScrollEvent({ 0, modifier }, {}, 'pixel'):post()
 end)
 
 local noiseCallback = {
-	[1] = bind(timer, timer.start),
+	[1] = function()
+		if hs.eventtap.checkKeyboardModifiers().cmd then 
+			modifier = 20
+		else
+			modifier = -20
+		end
+		timer:start()
+	end,
 	[2] = bind(timer, timer.stop),
 	[3] = function() hs.application.frontmostApplication():hide() end,
 }
