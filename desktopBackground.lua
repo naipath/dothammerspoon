3092efa814532b92d9bdf2eaf15d5e1190be73e0
tvs <teije-vs@hotmail.com>
2020-06-23 15:55:32 +0200

    Add png as option, remove redundant check

--- desktopBackground.lua
+++ desktopBackground.lua
@@ -9,16 +9,13 @@ hs.hotkey.bind(cah, "d", function()
     local totalFiles = 0
     local iterFn, dirObj = hs.fs.dir(picturesDirectory)
 
-    if iterFn then
-        for file in iterFn, dirObj do
-            if ends_with(file, ".jpg") then
-                totalFiles = totalFiles + 1
-                files[totalFiles] = file
-            end
+    for file in iterFn, dirObj do
+        if ends_with(file, ".jpg") or ends_with(file, ".png") then
+            totalFiles = totalFiles + 1
+            files[totalFiles] = file
         end
     end
 
     local background = "file://" .. picturesDirectory .. files[hs.math.randomFromRange(1, totalFiles)]
-    print(background)
     hs.screen.mainScreen():desktopImageURL(background)
 end)
